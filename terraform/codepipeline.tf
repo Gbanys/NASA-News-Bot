61762bdf193b4b384328d2d79378f9f7d3fee5ba
gbanys <giedriusbanys150@gmail.com>
2024-10-09 21:48:49 +0100

    Add push trigger to AWS codepipeline

--- terraform/codepipeline.tf
+++ terraform/codepipeline.tf
@@ -1,6 +1,7 @@
 resource "aws_codepipeline" "codepipeline" {
   name     = "nasa-pipeline"
   role_arn = aws_iam_role.nasa_codepipeline_role.arn
+  pipeline_type = "V2"
 
   artifact_store {
     location = aws_s3_bucket.nasa_codepipeline_bucket.bucket
@@ -9,6 +10,18 @@ resource "aws_codepipeline" "codepipeline" {
     encryption_key {
       id   = aws_kms_alias.nasa_s3_kms_alias.id
       type = "KMS"
+    }
+  }
+
+  trigger {
+    provider_type = "CodeStarSourceConnection"
+    git_configuration {
+      source_action_name = "Source"
+      push{
+        branches {
+          includes = ["master"]
+        }
+      }
     }
   }
 
@@ -69,96 +82,6 @@ resource "aws_codepipeline" "codepipeline" {
       }
     }
   }
-}
-
-resource "aws_codebuild_project" "nasa_codebuild_project" {
-  name          = "NasaCodeBuildProject"
-  description   = "A CodeBuild project for NASA news bot application"
-  build_timeout = 30 #
-
-  source {
-    type      = "GITHUB"
-    location  = "https://github.com/Gbanys/NASA-News-Bot.git" # Change to your GitHub repo URL
-    buildspec = "buildspec.yml" # Adjust if your buildspec is named differently
-  }
-
-  environment {
-    compute_type = "BUILD_GENERAL1_SMALL"
-    image        = "aws/codebuild/standard:5.0" # Adjust according to your needs
-    type         = "LINUX_CONTAINER"
-
-    environment_variable {
-      name  = "ENV_VAR_NAME"
-      value = "value"
-    }
-  }
-
-  service_role = aws_iam_role.nasa_codebuild_role.arn
-
-  artifacts {
-    type = "S3"
-    location = "${aws_s3_bucket.nasa_codepipeline_bucket.bucket}"
-    packaging = "ZIP"
-    name = "build-output.zip"
-    override_artifact_name = true
-  }
-}
-
-resource "aws_iam_role" "nasa_codebuild_role" {
-  name = "nasa_codebuild_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Principal = {
-        Service = "codebuild.amazonaws.com"
-      }
-      Effect = "Allow"
-      Sid    = ""
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "nasa_codebuild_policy" {
-  role       = aws_iam_role.nasa_codebuild_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildAdminAccess"
-}
-
-data "aws_iam_policy_document" "codebuild_logs_policy" {
-  statement {
-    effect = "Allow"
-    actions = [
-      "logs:CreateLogGroup",
-      "logs:CreateLogStream",
-      "logs:PutLogEvents",
-    ]
-    resources=["*"]
-  }
-  statement {
-    effect = "Allow"
-    actions = [
-      "s3:GetObject"
-    ]
-    resources=["arn:aws:s3:::nasa-codepipeline-bucket/*"]
-  }
-  statement {
-    effect = "Allow"
-    actions = [
-      "kms:Decrypt"
-    ]
-    resources=[aws_kms_key.nasa_s3_kms_key.arn]
-  }
-}
-
-resource "aws_iam_policy" "logs_policy" {
-  name   = "nasa_codepipeline_codebuild_logs_policy"
-  policy = data.aws_iam_policy_document.codebuild_logs_policy.json
-}
-
-resource "aws_iam_role_policy_attachment" "logs_policy_attachment" {
-  role       = aws_iam_role.nasa_codebuild_role.name
-  policy_arn = aws_iam_policy.logs_policy.arn
 }
 
 resource "aws_codestarconnections_connection" "nasa_repository_connection" {
